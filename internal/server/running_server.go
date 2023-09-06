da8d6249cda577b4ca6e649688d8607f6368b602
aecheist <lilapank@yandex.ru>
2023-09-06 17:27:53 +0600

    add path css and img

--- internal/server/running_server.go
+++ internal/server/running_server.go
@@ -6,13 +6,13 @@ import (
 )
 
 func Running_server() {
-	// init CSS
-	// http.Handle("/css/", http.StripPrefix("/css/", http.FileServer(http.Dir("./css/"))))
 	mux := http.NewServeMux()
+	mux.Handle("/css/", http.StripPrefix("/css/", http.FileServer(http.Dir("css/"))))
+	mux.Handle("/img/", http.StripPrefix("/img/", http.FileServer(http.Dir("img/"))))
 	mux.HandleFunc("/", handler_index)
 	mux.HandleFunc("/ascii-art", handler_ascii_art)
-	log.Println("Listening on: http://localhost:8080/")
-	log.Fatal(http.ListenAndServe(":8080", mux))
+	log.Println("Listening on: http://localhost:5080/")
+	log.Fatal(http.ListenAndServe(":5080", mux))
 	// httpServer := exec.Command("xdg-open", "http://localhost:8080")
 	// if err := httpServer.Run(); err != nil {
 	// 	panic(err)
