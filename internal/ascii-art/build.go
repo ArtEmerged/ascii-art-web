be164e22c17b7fd2cabf7e93ed9ab6ef378a8c2c
Artyom_HP(WSL) <lilapank@yandex.ru>
2023-09-22 23:33:30 +0600

    add log

--- internal/ascii-art/build.go
+++ internal/ascii-art/build.go
@@ -1,17 +1,29 @@
 package asciiart
 
-func Build(text, fs string) (string, error) {
+import (
+	"log"
+	"net/http"
+)
+
+func Build(text, fs string) (string, int) {
 	if err := AsciiChar(text); err != nil {
-		return "", err
+		log.Println(err)
+		return "", http.StatusBadRequest
+	}
+	if fs != "standard" && fs != "shadow" && fs != "thinkertoy" {
+		log.Println("User requests an incorrect banner")
+		return "", http.StatusBadRequest
 	}
 	if err := Validate(); err != nil {
-		return "", err
+		log.Println(err)
+		return "", http.StatusInternalServerError
 	}
 	fsChar, err := AsciiList(fs)
 	if err != nil {
-		return "", err
+		log.Println(err)
+		return "", http.StatusInternalServerError
 	}
 
 	split_text := Split(text)
-	return Print(split_text, fsChar), nil
+	return Print(split_text, fsChar), 0
 }
