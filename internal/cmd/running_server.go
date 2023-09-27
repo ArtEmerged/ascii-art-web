5741c7d36b8f8cff94f5740e6440052dfbdf2b32
aecheist <lilapank@yandex.ru>
2023-09-27 16:56:33 +0600

    rm NewMux

--- internal/cmd/running_server.go
+++ internal/cmd/running_server.go
@@ -13,10 +13,9 @@ func init() {
 }
 
 func Running_server() {
-	mux := http.NewServeMux()
-	mux.Handle("/static/", http.StripPrefix("/static/", http.FileServer(http.Dir("./web/static/"))))
-	mux.HandleFunc("/", handler_index)
-	mux.HandleFunc("/ascii-art", handler_ascii_art)
+	http.Handle("/static/", http.StripPrefix("/static/", http.FileServer(http.Dir("./web/static/"))))
+	http.HandleFunc("/", handler_index)
+	http.HandleFunc("/ascii-art", handler_ascii_art)
 	log.Println("Listening on: http://localhost:8080/")
-	log.Fatal(http.ListenAndServe(":8080", mux))
+	log.Fatal(http.ListenAndServe(":8080", nil))
 }
